2313c28f71afb2c231ca92ea614e925d7200344a
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2022-10-04 08:55:13 +0530

    Rearranged Files And Folders

---
+++ Lab1/ArrRev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n=6;
+    int arr[6] = {9, 8, 7, 2, 4, 3};
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+}
