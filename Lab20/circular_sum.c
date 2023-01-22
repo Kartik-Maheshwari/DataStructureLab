355606498cdaf8676ed8fc1bdf66645411727707
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2023-01-22 10:22:13 +0530

    Added circular_sum

--- Lab20/circular_sum.c
+++ Lab20/circular_sum.c
@@ -1,1 +1,33 @@
 //Program to Modify a Circular Linked List such that each node stores the sum of all nodes except itself
+#include"cir_linked_primitive.h"
+
+int total(Node *l1){
+    Node *curr=l1;
+    int sum=0;
+    do{
+        sum+=curr->val;
+        curr=curr->next;
+    }while(curr!=l1);
+    return sum;
+}
+
+void modify(Node **l1,int sum){
+    Node *curr=(*l1);
+    do{
+        curr->val = sum - curr->val;
+        curr=curr->next;
+    }while(curr!=(*l1));
+}
+
+int main(){
+    Node * last1=NULL;
+    insert_beg(&last1,5);
+    insert_beg(&last1,10);
+    insert_beg(&last1,20);
+    insert_last(&last1,11);
+    insert_last(&last1,45);
+    insert_last(&last1,55);
+    show(last1);
+    modify(&last1,total(last1));
+    show(last1);
+}
--- Lab20/reverse_circular.c
+++ Lab20/reverse_circular.c
@@ -2,12 +2,8 @@
 
 void reverse(Node** last)
 {
-    // if list is empty
     if (*last == NULL)
         return;
- 
-    // reverse procedure same as reversing a
-    // singly linked list
     Node* prev = NULL;
     Node* current = (*last)->next;
     Node* next;
@@ -17,11 +13,6 @@ void reverse(Node** last)
         prev = current;
         current = next;
     } while (current->next!=NULL);
- 
-    // adjusting the links so as to make the
-    // last node point to the first node
-    // (*last)->next = prev;
-    // *last = prev;
     current->next=(*last);
     (*last)=next;
 }
